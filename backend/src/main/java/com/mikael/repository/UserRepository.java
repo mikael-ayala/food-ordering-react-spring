026fed6583e6f191bca276193c6a3ba037a8794a
mikael-ayala <mikael.ayala.santos@gmail.com>
2025-02-19 20:34:19 -0300

    add UserRepository

---
+++ backend/src/main/java/com/mikael/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.mikael.repository;
+
+import com.mikael.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    public User findByEmail(String username);
+}
