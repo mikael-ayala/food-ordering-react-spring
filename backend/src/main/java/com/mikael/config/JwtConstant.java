ba3607f0622bca519a5c72c6488072690dceea51
mikael-ayala <mikael.ayala.santos@gmail.com>
2025-02-20 15:10:00 -0300

    add AppConfig and JwtTokenValidator

---
+++ backend/src/main/java/com/mikael/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.mikael.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "jçasldkjfaçsldkfjçasldkjfçasldkjfçalskjçlifjwqçofiasjdlfçjqwoiejfçaslkdj;.,zxcmva;kljg;lku;ilnuweyroqiuwerhjqf";
+    public static final String JWT_HEADER = "Authorization";
+}
